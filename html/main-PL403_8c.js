e9726e0a00f6310b246ab87f2a7ebbe29cd13f27
Rodriguinhozinho <rodrigomarques.rm862@gmail.com>
2025-12-14 09:17:33 +0000

    Documentaçao

---
+++ html/main-PL403_8c.js
@@ -0,0 +1,4 @@
+var main_PL403_8c =
+[
+    [ "main", "main-PL403_8c.html#af7ee5f82de9c9de8fb431b00ebaf78b5", null ]
+];
---
+++ html/navtreedata.js
@@ -0,0 +1,65 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PL4-03 Documentação do programa", "index.html", [
+    [ "Geral", "index.html#autotoc_md1", null ],
+    [ "Utilização", "index.html#autotoc_md2", null ],
+    [ "Navegação", "index.html#autotoc_md3", null ],
+    [ "funcionalidades", "funcionalidades.html", [
+      [ "Funcionalidades", "funcionalidades.html#autotoc_md7", [
+        [ "Manipulação de Vetores", "funcionalidades.html#autotoc_md8", [
+          [ "1.1. Ordenação e Simétrico", "funcionalidades.html#autotoc_md9", null ],
+          [ "1.2. Operações de Soma e Múltiplos", "funcionalidades.html#autotoc_md10", null ],
+          [ "1.3. Outras operações", "funcionalidades.html#autotoc_md11", null ]
+        ] ],
+        [ "2.Geração e Transposição de Matrizes", "funcionalidades.html#autotoc_md12", [
+          [ "2.1. Matrizes", "funcionalidades.html#autotoc_md13", null ],
+          [ "2.2. Transposição", "funcionalidades.html#autotoc_md14", null ]
+        ] ],
+        [ "3. Ajuda do Programa", "funcionalidades.html#autotoc_md15", null ]
+      ] ]
+    ] ],
+    [ "estrutura_arquivos", "estrutura_arquivos.html", [
+      [ "Arquivos do Projeto", "estrutura_arquivos.html#autotoc_md5", null ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"estrutura_arquivos.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
