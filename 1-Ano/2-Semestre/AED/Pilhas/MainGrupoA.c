84d1069a467b298c56f2391461517ac5fa9d0a50
Rodriguinhozinho <rodrigomarques.rm862@gmail.com>
2026-04-14 15:11:58 +0100

    exericicos pilhas

--- 1-Ano/2-Semestre/AED/Pilhas/MainGrupoA.c
+++ 1-Ano/2-Semestre/AED/Pilhas/MainGrupoA.c
@@ -9,7 +9,28 @@
 
 int main()
 {
-
-
+    PNodoPilha S;
+    int N = gerarNumeroInteiro(0,15);
+    S = criarPilha(N);
+    mostrarPilha(S);
+    int flag;
+    DadosPilha X;
+    /*flag = delvolvesec(&S, &X);
+    if (flag == 1){
+        printf("Segundo elemento:\n");
+        mostrarElementoPilha(X);
+        printf("Pilha:\n");
+        mostrarPilha(S);
+    }
+    else{
+        printf("A pilha não tem elementos suficientes para realizar a operação.\n");
+    }*/
+    flag = mostrarN(&S, &X, 3);
+    if (flag == 1){
+        printf("Terceiro elemento:\n");
+        mostrarElementoPilha(X);
+        printf("Pilha:\n");
+        mostrarPilha(S);
+    }
 }
 
--- 1-Ano/2-Semestre/AED/Pilhas/OutrasOperacoesGrupoA.h
+++ 1-Ano/2-Semestre/AED/Pilhas/OutrasOperacoesGrupoA.h
@@ -7,7 +7,7 @@
 //   - não faz parte da EAD Pilha
 //   - mostra a Pilha do topo para o fundo
 void mostrarPilha (PNodoPilha);
-
+int mostrarN(PNodoPilha *, DadosPilha*, int N);
 
 
 
@@ -28,3 +28,25 @@ void mostrarPilha (PNodoPilha S)
 }
 
 
+int mostrarN(PNodoPilha* S, DadosPilha* X, int N)
+{
+  if(pilhaVazia(*S))
+    return -1; // Pilha vazia
+  PNodoPilha S2 = criarPilha();
+  int i = 1;
+  while (i < N && !pilhaVazia(*S)){
+    S2 = push(topo(*S), S2);
+    *S = pop(*S);
+    i++;
+  }
+  int resultado = 0;
+  if (!pilhaVazia(*S)){
+    *X = topo(*S);
+    resultado = 1;
+  }
+  while (!pilhaVazia(S2)){
+    *S = push(topo(S2), *S);
+    S2 = pop(S2);
+  }
+  return resultado;
+}
