279ae42d7675c2ee78167b5386df9a30c7106fad
Rodriguinhozinho <rodrigomarques.rm862@gmail.com>
2026-04-21 14:20:07 +0100

    Arvores Binarias

---
+++ 1-Ano/2-Semestre/AED/ArvoresBinarias/MainAB.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Aleatorio.h"
+#include "OperacoesBasicasAB.h"
+#include "EADArvoreBinaria.h"
+#include "ABPorNiveis.h"
+#include "OutrasOperacoesAB.h"
+
+int main()
+{
+
+
+}
